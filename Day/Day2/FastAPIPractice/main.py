1578297225b6e9bdfc57386a5fac215e1e1ba77e
bharati-cse <2025cs_bharatikamalakshagoudapatil_b@nie.ac.in>
2026-09-22 13:20:42 +0530

    Path Paramiters

--- Day/Day2/FastAPIPractice/main.py
+++ Day/Day2/FastAPIPractice/main.py
@@ -13,7 +13,10 @@ def health():
 @app.post("/create")
 def create_something():
     return {"message": "created"}
-
+#Path parameters
+@app.get("/students/{usn}")
+def get_result(usn):
+    return {"Result":"Distinction","usn":usn}
 
 ##HTTP methods
 #Are a way to talk (from browser) to the server
